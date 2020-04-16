cabfd08ab19f0d3e324ed9ccc0960349a771dbaf
kle043 <19959498+kle043@users.noreply.github.com>
2020-04-17 00:00:50 +0200

    add tests for unmarshalling files

---
+++ tests/test_read_bindings.py
@@ -0,0 +1,22 @@
+import os
+import pytest
+from witsdm.read_bindings import witsml
+
+sample_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'samples')
+
+@pytest.mark.parametrize("test_filename", ["log.xml", 
+                                           "message.xml", 
+                                           "mudLog.xml", 
+                                           "risk.xml", 
+                                           "trajectory.xml", 
+                                           "wellbore.xml"])
+def test_eval(test_filename):
+    print(os.path.join(sample_path, test_filename))
+    with open(os.path.join(sample_path, test_filename), 'r') as test_file:
+        obj = pywitsml.CreateFromDocument(test_file.read())
+
+
+
+
+
+
