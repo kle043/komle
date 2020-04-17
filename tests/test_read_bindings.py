4e7e1694d70d4c798a3f9b0258b77e74cab94e96
kle043 <19959498+kle043@users.noreply.github.com>
2020-04-17 09:55:00 +0200

    fix indent

--- tests/test_read_bindings.py
+++ tests/test_read_bindings.py
@@ -4,17 +4,17 @@ from komle.read_bindings import witsml
 
 sample_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'samples')
 
-@pytest.mark.parametrize("test_filename", ["log.xml", 
-                                           "message.xml", 
-                                           "mudLog.xml", 
-                                           "risk.xml", 
-                                           "trajectory.xml", 
-                                           "wellbore.xml"])
+@pytest.mark.parametrize("test_filename", 
+                        ["log.xml", 
+                         "message.xml", 
+                         "mudLog.xml", 
+                         "risk.xml", 
+                         "trajectory.xml", 
+                         "wellbore.xml"])
 def test_unmarshalling(test_filename):
-''' Test that unmarshalling energistics well A files goes fine
-'''
+    '''Test unmarshalling energistics well A test files'''
     with open(os.path.join(sample_path, test_filename), 'r') as test_file:
-        obj = pywitsml.CreateFromDocument(test_file.read())
+        obj = witsml.CreateFromDocument(test_file.read())
 
 
 
