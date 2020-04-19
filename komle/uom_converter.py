a6e5df9a08a6e984f176c19de4d02f439421b766
kle043 <19959498+kle043@users.noreply.github.com>
2020-04-19 19:09:14 +0200

    add support for denominator

--- komle/uom_converter.py
+++ komle/uom_converter.py
@@ -4,34 +4,48 @@ from komle.uom_bindings import uom
 with open(os.path.join(os.path.dirname(__file__), "witsmlUnitDict.xml"), 'r') as uom_file:
     WITSM_UNIT_DICT = uom.CreateFromDocument(uom_file.read())
 
-def conversion_factor(source: str, target: str='base') -> float:
-    '''Retrieve a witsml convertion factor for a given unit to a target unit.
+def __get_factor(unit: uom.unitDictionaryType):
+
+    factor = 1.0
+
+    if unit.ConversionToBaseUnit is not None:
+        if unit.ConversionToBaseUnit.Factor is not None:
+            factor = unit.ConversionToBaseUnit.Factor
+        elif unit.ConversionToBaseUnit.Fraction.Numerator is not None and unit.ConversionToBaseUnit.Fraction.Denominator is not None:
+            factor = unit.ConversionToBaseUnit.Fraction.Numerator / unit.ConversionToBaseUnit.Fraction.Denominator
+
+    elif unit.BaseUnit is None:
+
+        raise ValueError(f'{unit.id} does not have a conversion to base unit')
+    
+    return factor
+    
+def conversion_factor(source_uom: str, target_uom: str) -> float:
+    '''Retrieve a witsml convertion factor for a given unit to a target_uom unit.
 
     Uses the witsml unit dict as a lookup of unit conversion factors
 
     Args:
-        source (str): id of the source witsml uom
-        targer (str): id of target uom, default returns base unit
+        source_uom (str): annotation of the source_uom for example m, ft, m/s, km/h etc
+        target_uom (str): annotation of target_uom for example m, ft,m/s, km/h etc
 
     Returns:
-        factor (float): multiplication factor for conversion
+        factor (float): multiplication factor for conversion from source_uom
     '''
+
     source_unit = target_unit = None
 
     for unit in WITSM_UNIT_DICT.UnitsDefinition.UnitOfMeasure:
-        if unit.id == source:
+        if unit.annotation == source_uom:
             source_unit = unit
-        elif unit.id == target:
+        elif unit.annotation == target_uom:
             target_unit = unit
 
-    source_factor = target_factor = 1.0
+    if source_unit is None or target_unit is None:
+        raise KeyError("Unit not found")
 
-    if source_unit.ConversionToBaseUnit is not None:
-        source_factor = source_unit.ConversionToBaseUnit.Factor
-
-    if target_unit.ConversionToBaseUnit is not None:
-        target_factor = target_unit.ConversionToBaseUnit.Factor
-
-
+    source_factor = __get_factor(source_unit)
+    target_factor = __get_factor(target_unit)
+    print(source_factor, target_factor)
     return source_factor / target_factor
 
