5edc6be854e4bd583bb602e5cb2a4a5176f646ae
kle043 <19959498+kle043@users.noreply.github.com>
2020-04-19 17:00:15 +0200

    pass tests

--- komle/uom_converter.py
+++ komle/uom_converter.py
@@ -2,7 +2,7 @@ import os
 from komle.uom_bindings import uom
 
 with open(os.path.join(os.path.dirname(__file__), "witsmlUnitDict.xml"), 'r') as uom_file:
-    witsm_unit_dict = uom.CreateFromDocument(uom_file.read())
+    WITSM_UNIT_DICT = uom.CreateFromDocument(uom_file.read())
 
 def conversion_factor(source: str, target: str='base') -> float:
     '''Retrieve a witsml convertion factor for a given unit to a target unit.
@@ -16,6 +16,22 @@ def conversion_factor(source: str, target: str='base') -> float:
     Returns:
         factor (float): multiplication factor for conversion
     '''
+    source_unit = target_unit = None
 
-    return 0.0
+    for unit in WITSM_UNIT_DICT.UnitsDefinition.UnitOfMeasure:
+        if unit.id == source:
+            source_unit = unit
+        elif unit.id == target:
+            target_unit = unit
+
+    source_factor = target_factor = 1.0
+
+    if source_unit.ConversionToBaseUnit is not None:
+        source_factor = source_unit.ConversionToBaseUnit.Factor
+
+    if target_unit.ConversionToBaseUnit is not None:
+        target_factor = target_unit.ConversionToBaseUnit.Factor
+
+
+    return source_factor / target_factor
 
