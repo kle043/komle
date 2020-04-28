26be90aa51ac0d2410d530640b8dbde37a8b0052
kle043 <19959498+kle043@users.noreply.github.com>
2020-04-28 14:44:18 +0200

    add write example

--- examples/hello_writer.py
+++ examples/hello_writer.py
@@ -1,7 +1,14 @@
-#%%
+#%% Import the write bindings
+# Note that from komle.read_bindings can not be used 
+# in the same session as they use the same namespace
+
 from komle.write_bindings import witsml
 
-# %%
+# %% Create logs, a container for obj_log
+ 
+logs = witsml.logs(version=witsml.__version__)
+
+# %% Create a log
 
 log = witsml.obj_log(uidWell='1', 
                      uidWellbore='2', 
@@ -10,14 +17,33 @@ log = witsml.obj_log(uidWell='1',
                      nameWellbore='SomeWellbore',
                      name='SomeName')
 
-# %%
-logs = witsml.logs(version=witsml.__version__)
 
-# %%
+
+# %% Append the log 
 logs.append(log)
 
-# %%
+# %% Write the logs object
 
+with open('out-log.xml', 'w') as log_file:
+    log_file.write(logs.toDOM().toprettyxml())
+
+# %% Add an log that is not complete
+
+logs.append(witsml.obj_log(uidWell='4', 
+                           uidWellbore='5', 
+                           uid='6',
+                           nameWell='SomeWell2',
+                           name='SomeName2'))
+
+# %% Try to write to file
+# This will give an error due to the missing nameWellbore
+
+with open('out-log.xml', 'w') as log_file:
+    log_file.write(logs.toDOM().toprettyxml())
+
+# %% Set the missing value and try again
+
+logs.log[1].nameWellbore = "SomeWellbore2"
 with open('out-log.xml', 'w') as log_file:
     log_file.write(logs.toDOM().toprettyxml())
 
