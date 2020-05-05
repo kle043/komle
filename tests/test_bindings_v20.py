c35e68115eabcf702f252b3d326657047a8479d2
kle043 <19959498+kle043@users.noreply.github.com>
2020-05-05 11:30:14 +0200

    add test of bindings

---
+++ tests/test_bindings_v20.py
@@ -0,0 +1,20 @@
+import os
+import pytest
+from komle.bindings.v20 import witsml
+
+sample_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'samples')
+
+@pytest.mark.parametrize("test_filename", 
+                        ["Log.xml", 
+                         "MudLogReport.xml"
+                        ])
+def test_unmarshalling(test_filename):
+    '''Test unmarshalling energistics well A test files'''
+    with open(os.path.join(sample_path, test_filename), 'r') as test_file:
+        obj = witsml.CreateFromDocument(test_file.read())
+
+
+
+
+
+
