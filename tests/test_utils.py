2c4735897b7aa2f07a41f17da6c905f9e4588d2f
HemersonRafael <rafaelpontes1995@gmail.com>
2022-02-22 08:47:52 -0300

    Reformatting test files.

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,26 +1,30 @@
 import os
-import pytest
 from datetime import datetime
-from komle.bindings.v1411.read import witsml
+
+import pytest
+
 from komle import utils as ku
+from komle.bindings.v1411.read import witsml
 
-sample_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'samples')
+sample_path = os.path.join(
+    os.path.dirname(os.path.realpath(__file__)), 'samples'
+)
 
 
-@pytest.mark.parametrize("test_filename", 
-                        ["log.xml",  
-                         'small_depth_log.xml'])
+@pytest.mark.parametrize('test_filename', ['log.xml', 'small_depth_log.xml'])
 def test_log_to_dict(test_filename):
 
     with open(os.path.join(sample_path, test_filename), 'r') as test_file:
         logs = witsml.CreateFromDocument(test_file.read())
-    
+
     logdata_frame = ku.logdata_dict(logs.log[0])
 
     for mnem in logs.log[0].logData[0].mnemonicList.split(','):
         assert mnem in logdata_frame
-    
-    assert len(list(logdata_frame.values())[0]) == len(logs.log[0].logData[0].data)
+
+    assert len(list(logdata_frame.values())[0]) == len(
+        logs.log[0].logData[0].data
+    )
 
     if logs.log[0].indexType == 'measured depth':
         assert isinstance(logdata_frame[logs.log[0].indexCurve][0], float)
@@ -28,12 +32,11 @@ def test_log_to_dict(test_filename):
         assert isinstance(logdata_frame[logs.log[0].indexCurve][0], datetime)
 
 
-
 def test_obj_dict_risk():
 
     with open(os.path.join(sample_path, 'risk.xml'), 'r') as test_file:
         obj = witsml.CreateFromDocument(test_file.read())
-    
+
     flatt_witsml = ku.obj_dict(obj.risk[0], True)
 
     assert flatt_witsml['mitigation[0]'] == 'Call the boss'
@@ -42,39 +45,59 @@ def test_obj_dict_risk():
     assert flatt_witsml['nameWellbore'] == 'Wellbore Test Bruce'
     assert len(flatt_witsml) == 45
 
+
 def test_obj_dict_mudLog():
 
     with open(os.path.join(sample_path, 'mudLog.xml'), 'r') as test_file:
         obj = witsml.CreateFromDocument(test_file.read())
-    
+
     print(type(obj))
     flatt_witsml = ku.obj_dict(obj.mudLog[0], True)
     assert flatt_witsml['commonData.comments'] == 'MudLog Object'
     assert len(flatt_witsml) == 1244
 
+
 def test_plural_dict_geoIntervall():
 
     with open(os.path.join(sample_path, 'mudLog.xml'), 'r') as test_file:
         obj = witsml.CreateFromDocument(test_file.read())
-    
+
     geo_frame = ku.plural_dict(obj.mudLog[0].geologyInterval, True)
-    
-    assert geo_frame['mdTop'][0:6] == [1860.0, 1870.0, 1880.0, 1890.0, 1890.0, 1900.0]
+
+    assert geo_frame['mdTop'][0:6] == [
+        1860.0,
+        1870.0,
+        1880.0,
+        1890.0,
+        1890.0,
+        1900.0,
+    ]
     assert len(geo_frame) == 28
+
 
 def test_plural_dict_lithology():
 
     with open(os.path.join(sample_path, 'mudLog.xml'), 'r') as test_file:
         obj = witsml.CreateFromDocument(test_file.read())
-    
-    lit_frame = ku.plural_dict(obj.mudLog[0].geologyInterval[1].lithology, True)
-    lit_frame_no_attr = ku.plural_dict(obj.mudLog[0].geologyInterval[1].lithology, False)
-    
+
+    lit_frame = ku.plural_dict(
+        obj.mudLog[0].geologyInterval[1].lithology, True
+    )
+    lit_frame_no_attr = ku.plural_dict(
+        obj.mudLog[0].geologyInterval[1].lithology, False
+    )
+
     for lit in [lit_frame, lit_frame_no_attr]:
         assert lit['lithPc'] == [70.0, 30.0]
         assert lit['codeLith'] == ['300', '405']
-        assert lit['description'] == ['Clay Clay-shale [Symbol not found, used by default]', 'Sand Fine sandstone']
+        assert lit['description'] == [
+            'Clay Clay-shale [Symbol not found, used by default]',
+            'Sand Fine sandstone',
+        ]
 
-    assert lit_frame['uid'] == ["6e106c6c-07f1-4288-b1d3-f3238606c1a8-Lith0", "6e106c6c-07f1-4288-b1d3-f3238606c1a8-Lith1"]
+    assert lit_frame['uid'] == [
+        '6e106c6c-07f1-4288-b1d3-f3238606c1a8-Lith0',
+        '6e106c6c-07f1-4288-b1d3-f3238606c1a8-Lith1',
+    ]
     assert len(lit_frame) == 6
     assert len(lit_frame_no_attr) == 4
--- tests/test_write_bindings.py
+++ tests/test_write_bindings.py
@@ -1,46 +1,53 @@
 import pytest
 from pyxb.namespace import Namespace, utility
 
-@pytest.fixture(scope="session", autouse=True)
+
+@pytest.fixture(scope='session', autouse=True)
 def setup():
     # If running all the tests at once, we can't have two equal namespaces
     # So reset in case read_bindings is loaded and load the write_binding
     for name in utility.AvailableNamespaces():
         getattr(super(Namespace, name), '_reset', lambda *args, **kw: None)()
-        name._Namespace__initialNamespaceContext =  None
+        name._Namespace__initialNamespaceContext = None
 
     yield
     # Reset namespace after test is finished
     for name in utility.AvailableNamespaces():
         getattr(super(Namespace, name), '_reset', lambda *args, **kw: None)()
-        name._Namespace__initialNamespaceContext =  None
+        name._Namespace__initialNamespaceContext = None
 
 
-    
 def test_logs():
     from komle.bindings.v1411.write import witsml
+
     logs = witsml.logs(version=witsml.__version__)
-    log = witsml.obj_log(uidWell='1', 
-                         uidWellbore='2', 
-                         uid='3',
-                         nameWell='SomeWell',
-                         nameWellbore='SomeWellbore',
-                         name='SomeName')
-    
+    log = witsml.obj_log(
+        uidWell='1',
+        uidWellbore='2',
+        uid='3',
+        nameWell='SomeWell',
+        nameWellbore='SomeWellbore',
+        name='SomeName',
+    )
+
     logs.append(log)
-    
+
     logs.toDOM()
+
 
 def test_trajecorys():
     from komle.bindings.v1411.write import witsml
+
     trajs = witsml.trajectorys(version=witsml.__version__)
 
-    traj = witsml.obj_trajectory(uidWell='4', 
-                                 uidWellbore='5', 
-                                 uid='6',
-                                 nameWell='SomeWell2',
-                                 nameWellbore='SomeWellbore',
-                                 name='SomeName2')
+    traj = witsml.obj_trajectory(
+        uidWell='4',
+        uidWellbore='5',
+        uid='6',
+        nameWell='SomeWell2',
+        nameWellbore='SomeWellbore',
+        name='SomeName2',
+    )
     traj.objectGrowing = False
     traj.mdMn = 0
     traj.mdMn.uom = 'm'
@@ -50,7 +57,7 @@ def test_trajecorys():
     traj_station = witsml.cs_trajectoryStation(uid='myuid')
     traj_station.typeTrajStation = 'unknown'
     traj_station.md, traj_station.md.uom = 40, 'm'
-    
+
     traj_station.tvd, traj_station.tvd.uom = 40.1, 'm'
     traj_station.incl, traj_station.incl.uom = 0.2, 'dega'
     traj_station.azi, traj_station.azi.uom = 0.1, 'dega'
@@ -58,7 +65,7 @@ def test_trajecorys():
     traj_station.dispEw, traj_station.dispEw.uom = 2.2, 'm'
     traj_station.vertSect, traj_station.vertSect.uom = 0, 'm'
     traj_station.dls, traj_station.dls.uom = 0, 'dega/m'
-    
+
     traj.trajectoryStation.append(traj_station)
     trajs.append(traj)
     trajs.toDOM()
