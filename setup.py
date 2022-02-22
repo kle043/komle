993f28fc9ef11133d81c4b6e66e0be61eb496fd8
HemersonRafael <rafaelpontes1995@gmail.com>
2022-02-22 09:33:58 -0300

    Reformatting  setup.py file and altering requires to install and tests.

--- setup.py
+++ setup.py
@@ -1,22 +1,20 @@
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
 setup(
-      name='komle',
-      version='0.3',
-      description='A python library to help with WITSML v1.4.1.1 and v2.0',
-      url='https://github.com/kle043/komle',
-      packages=find_packages(exclude=('tests',)),
-      author='kle043',
-      author_email='pale.dorg@gmail.com',
-      include_package_data=True,
-      package_data={
-                    'komle': ['WMLS.WSDL', 'witsmlUnitDict.xml'],
-                   },
-      install_requires=['suds-py3==1.4.0.0',
-                        'PyXB==1.2.6',
-
-                       ], 
-      tests_require=[
-                     'pytest>=3.0'
-                    ]
-    )
+    name="komle",
+    version="0.3",
+    description="A python library to help with WITSML v1.4.1.1 and v2.0",
+    url="https://github.com/kle043/komle",
+    packages=find_packages(exclude=("tests",)),
+    author="kle043",
+    author_email="pale.dorg@gmail.com",
+    include_package_data=True,
+    package_data={
+        "komle": ["WMLS.WSDL", "witsmlUnitDict.xml"],
+    },
+    install_requires=[
+        "suds-py3>=1.4.5",
+        "PyXB>=1.2.6",
+    ],
+    tests_require=["pytest>=7.0.1"],
+)
